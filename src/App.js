c6d034761b850fe03f875bb8a9a4e426cb9c3b1e
Aekansh Kansal <aekansh.kansal@phonepe.com>
2022-12-09 03:19:48 +0530

    fixed ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 import SpeechRecognition, {
   useSpeechRecognition,
 } from "react-speech-recognition";
 import "./App.scss";
 import Mic from "./icons/mic.png";
 import Mute from "./icons/mute.png";
+import { IoMdSend } from 'react-icons/io';
 import { interact } from "./apis/Server";
+import monster_img from './icons/monster_still1.png';
+import { IconContext } from 'react-icons'
+
+
+let allVoices = []
+const initSpeech = new SpeechSynthesisUtterance();
+window.speechSynthesis.onvoiceschanged = () => {
+  allVoices = window.speechSynthesis.getVoices();
+  console.log(allVoices);
+};
+
+const speechUtteranceChunker = function (utt, settings, callback) {
+  settings = settings || {};
+  let newUtt;
+  let txt = (settings && settings.offset !== undefined ? utt.text.substring(settings.offset) : utt.text);
+  if (utt.voice && utt.voice.voiceURI === 'native') { // Not part of the spec
+      newUtt = utt;
+      newUtt.text = txt;
+      newUtt.addEventListener('end', function () {
+          if (speechUtteranceChunker.cancel) {
+              speechUtteranceChunker.cancel = false;
+          }
+          if (callback !== undefined) {
+              callback();
+          }
+      });
+  }
+  else {
+      let chunkLength = (settings && settings.chunkLength) || 160;
+      let pattRegex = new RegExp('^[\\s\\S]{' + Math.floor(chunkLength / 2) + ',' + chunkLength + '}[.!?,]{1}|^[\\s\\S]{1,' + chunkLength + '}$|^[\\s\\S]{1,' + chunkLength + '} ');
+      let chunkArr = txt.match(pattRegex);
+      if (chunkArr[0] === undefined || chunkArr[0].length <= 2) {
+          //call once all text has been spoken...
+          if (callback !== undefined) {
+              callback();
+          }
+          return;
+      }
+      let chunk = chunkArr[0];
+      newUtt = new SpeechSynthesisUtterance(chunk);
+      let x;
+      for (x in utt) {
+          if (utt.hasOwnProperty(x) && x !== 'text') {
+              newUtt[x] = utt[x];
+          }
+      }
+      newUtt.addEventListener('end', function () {
+          if (speechUtteranceChunker.cancel) {
+              speechUtteranceChunker.cancel = false;
+              return;
+          }
+          settings.offset = settings.offset || 0;
+          settings.offset += chunk.length - 1;
+          speechUtteranceChunker(utt, settings, callback);
+      });
+  }
+  if (settings.modifier) {
+      settings.modifier(newUtt);
+  }
+  console.log(newUtt); //IMPORTANT!! Do not remove: Logging the object out fixes some onend firing issues.
+  //placing the speak invocation inside a callback fixes ordering and onend issues.
+  setTimeout(function () {
+      newUtt.voice = allVoices[145];
+      speechSynthesis.speak(newUtt);
+  }, 0);
+};
 
 function App() {
   const {
@@ -14,6 +81,21 @@ function App() {
     resetTranscript,
     browserSupportsSpeechRecognition,
   } = useSpeechRecognition();
+
+  function speak(message) {
+    const speech = new SpeechSynthesisUtterance(message);
+    const voiceArr = window.speechSynthesis.getVoices();
+    speech.voice = voiceArr[145];
+    
+    speechUtteranceChunker(speech, {
+      chunkLength: 120
+    }, function() {
+      console.log('spoken')
+      setIsSpeaking(false);
+    });
+  }
+  
+  const [isSpeaking, setIsSpeaking] = useState(false);
   const messagesEndRef = React.useRef(null)
   const [transcriptText, setTranscriptText] = React.useState("");
   const [loading, setLoading] = React.useState(false);
@@ -38,15 +120,18 @@ function App() {
   }
 
   const handleSend = async () => {
+    setIsSpeaking(false);
     const payloadText = transcriptText.trim();
     setTranscriptText("");
     resetTranscript();
     setLoading(true);
-
     setConversation(conversation => [...conversation, {self: true, time: `${new Date().getHours()}:${new Date().getMinutes()}`, text: payloadText}])
 
     const message = await interact(payloadText);
-  
+  	    
+    //text to speech
+    setIsSpeaking(true);
+    speak(message);
     setLoading(false);
     setConversation(conversation => [...conversation, {self: false, time: `${new Date().getHours()}:${new Date().getMinutes()}`, text: message}])
   };
@@ -67,7 +152,7 @@ function App() {
     <>
       <div className="chat">
         <div className="chat-title">
-          <h1>Hedylogos</h1>
+          <h1>PhonePe Insurance</h1>
           <h2>Online</h2>
           <figure className="avatar">
             <img src="https://logos-download.com/wp-content/uploads/2021/01/PhonePe_Logo.png" alt=""/>
@@ -75,6 +160,28 @@ function App() {
         </div>
 
         <div className="messages">
+          <div className="smiley">
+
+            <div class="main">
+              <div class="monster">
+              { isSpeaking ? 
+                              <div class="monster__face">
+                              <div class="monster__eyes">
+                                <div class="monster__eye"></div>
+                                <div class="monster__eye"></div>
+                              </div>
+                              <div class="monster__mouth">
+                                <div class="monster__top"></div>
+                                <div class="monster__bottom"></div>
+                              </div>
+                            </div>
+              :
+                    <img src={monster_img} className="still_monster" />
+          }
+              </div>
+            </div>
+          </div>
+
           <div className="messages-content">
             {conversation.map((message, index) => {
               return message.self ? (
@@ -117,7 +224,12 @@ function App() {
 
           {transcriptText && (
             <button type="submit" className="message-send" onClick={handleSend}>
-              {"Send"}
+                  <IconContext.Provider
+      value={{ size: '25px' }}
+    >
+      <IoMdSend />
+    </IconContext.Provider>
+
             </button>
           )}
           <button
@@ -128,7 +240,7 @@ function App() {
           >
             <img
               src={listening ? Mute : Mic}
-              style={{ height: 12, width: 12 }}
+              style={{ height: 24, width: 24 }}
               alt=""
             />
           </button>
